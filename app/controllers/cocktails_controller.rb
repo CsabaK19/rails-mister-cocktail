e60e0f42a8aab47b64b132e918b6b7e89a99c341
Csaba Krisztan <krisztan.csaba@gmail.com>
2020-01-30 21:27:30 +0000

    cocktails front

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,6 +21,12 @@ class CocktailsController < ApplicationController
     @dose = Dose.new
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
   private
 
   def cocktail_params
